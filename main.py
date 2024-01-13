aee658e791c07c898c699648adf768572b2d2807
4ymazik <alankirpichev760@gmail.com>
2024-01-13 14:22:26 +0300

    Создан фон и анимированная лягушка

---
+++ main.py
@@ -0,0 +1,85 @@
+import pygame
+import sys
+import os
+
+
+def main():
+    pygame.init()
+    size = width, height = 1000, 600
+    screen = pygame.display.set_mode(size)
+
+    all_sprites = pygame.sprite.Group()
+
+    swamp_image = load_image("swamp.png")
+    swamp = pygame.sprite.Sprite(all_sprites)
+    swamp.image = swamp_image
+    swamp.rect = swamp.image.get_rect()
+
+    sheet = pygame.image.load("data/frog_idle.png").convert_alpha()
+    frog = AnimatedSprite(sheet, 8, 1, 100, 100, all_sprites)
+    frog.rect.x = 400
+    frog.rect.y = 400
+
+    fps = 10
+    clock = pygame.time.Clock()
+
+    running = True
+    while running:
+        all_sprites.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        all_sprites.draw(screen)
+        clock.tick(fps)
+        pygame.display.flip()
+
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+
+    pygame.quit()
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y, group, color_key=None):
+        super().__init__(group)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+        self.image = self.image.convert_alpha()
+
+
+if __name__ == '__main__':
+    main()
