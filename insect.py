8e3cd672e8541fc8d37ceaad189797f46b41b0bb
4ymazik <alankirpichev760@gmail.com>
2024-01-30 20:08:03 +0300

    класс анимированного спрайта в отдельный файл и там еще че то

--- insect.py
+++ insect.py
@@ -1,8 +1,9 @@
 import pygame
 import main
+import animated_sprite
 
 
-class Insect(main.AnimatedSprite):
+class Insect(animated_sprite.AnimatedSprite):
     def __init__(self, sheet, columns, rows, x, y, group):
         super().__init__(sheet, columns, rows, x, y, group)
         self.vx = 20
@@ -10,6 +11,17 @@ class Insect(main.AnimatedSprite):
 
     def move(self):
         self.rect = self.rect.move(self.vx, self.vy)
+        if self.rect[0] > 400 and self.rect[1] < 130:
+            self.vx = 10
+            self.vy = 10
+        elif self.rect[0] > 550 and 150 < self.rect[1] < 200:
+            self.vx = 0
+            self.vy = 10
+        elif self.rect[1] > 200:
+            self.vx = -10
+            self.vy = -8
+        print(self.rect)
+
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 import insect
+import animated_sprite
 
 
 def load_image(name, color_key=None):
@@ -20,31 +21,7 @@ def load_image(name, color_key=None):
     return image
 
 
-class AnimatedSprite(pygame.sprite.Sprite):
-    def __init__(self, sheet, columns, rows, x, y, group):
-        super().__init__(group)
-        self.frames = []
-        self.cut_sheet(sheet, columns, rows)
-        self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
-
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
-
-    def update(self):
-        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-        self.image = self.frames[self.cur_frame]
-        self.image = self.image.convert_alpha()
-
-
-class Frog(AnimatedSprite):
+class Frog(animated_sprite.AnimatedSprite):
     def __init__(self, sheet, columns, rows, x, y, group):
         super().__init__(sheet, columns, rows, x, y, group)
 
@@ -82,12 +59,13 @@ def main():
     swamp.rect = swamp.image.get_rect()
 
     frog_sheet = pygame.image.load("data/frog_idle.png").convert_alpha()
-    frog = Frog(frog_sheet, 8, 1, 100, 100, all_sprites)
+    frog_sheet2 = pygame.image.load("data/frog_idle2.png").convert_alpha()
+    frog = Frog(frog_sheet,8, 1, 100, 100, all_sprites)
     frog.rect.x = 400
     frog.rect.y = 400
 
     beetle_sheet = pygame.image.load("data/beetle_move_right.png").convert_alpha()
-    beetle_sheet2 = pygame.image.load("data/beetle_move_right.png").convert_alpha()
+    beetle_sheet2 = pygame.image.load("data/beetle_move_left.png").convert_alpha()
     beetle = insect.Insect(beetle_sheet, 4, 1, 100, 100, all_sprites)
 
     fps = 8
